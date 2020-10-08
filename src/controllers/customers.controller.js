5049c1c47187537f093550a84ad3619a1084a670
LaraIsaac <laragr.isaac@gmail.com>
2020-10-08 12:10:35 -0500

    Update to getbyId

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -3,9 +3,36 @@ import jwt from 'jsonwebtoken'
 import config from '../config'
 
 export const getCustomerById = async (req, res) => {
-    Customer.find({}).exec(function(err, customerfile){
-        if( err ){ console.log('Error: ', err); return; }
-        console.log("The INDEX");
-        res.render('../views/product/index', {customer: customerfile} );
         
+    Customer.findOne({ username: username }, function(err, user) {
+        if (err) { 
+            console.log('Error: ', err);
+          // user not found 
+        return res.send(401);
+        }
+  
+        if (!user) {
+        // incorrect username
+        return res.send(401);
+        }
+  
+        if (!user.validPassword(password)) {
+        // incorrect password
+        return res.send(401);
+        }
+        // User has authenticated OK
+        res.send(200);
+    });
+
+    var expires = moment().add('days', 7).valueOf();
+    var token = jwt.encode({
+      iss: user.id,
+      exp: expires
+    }, app.get('jwtTokenSecret'));
+    
+    res.json({
+      token : token,
+      expires: expires,
+      user: user.toJSON()
+    });
 }
